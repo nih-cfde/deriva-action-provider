9953e281a8fc40de2b915a66c4042908b4032cd9
Nickolaus D. Saint <nick@globus.org>
2021-01-13 15:58:58 -0800

    Remove old user auth functionality

--- cfde_ap/auth.py
+++ cfde_ap/auth.py
@@ -38,26 +38,3 @@ def get_webauthn_user():
                 for attr in new_user_info['attributes']
             ]
     )
-
-
-def get_deriva_token():
-    # TODO: When decision is made about user auth vs. conf client auth, implement.
-    #       Currently using personal refresh token for scope.
-    #       See keys.py for last-generated date.
-
-    return globus_sdk.RefreshTokenAuthorizer(
-                        refresh_token=CONFIG["TEMP_REFRESH_TOKEN"],
-                        auth_client=globus_sdk.NativeAppAuthClient(CONFIG["GLOBUS_NATIVE_APP"])
-           ).access_token
-
-
-def _generate_new_deriva_token():
-    # Generate new Refresh Token to be used in get_deriva_token()
-    native_client = globus_sdk.NativeAppAuthClient(CONFIG["GLOBUS_NATIVE_APP"])
-    native_flow = native_client.oauth2_start_flow(
-                                    requested_scopes=("https://auth.globus.org/scopes/"
-                                                      "app.nih-cfde.org/deriva_all"),
-                                    refresh_tokens=True)
-    code = input(f"Auth at '{native_flow.get_authorize_url()}' and paste code:\n")
-    tokens = native_flow.exchange_code_for_tokens(code)
-    return tokens["refresh_token"]
