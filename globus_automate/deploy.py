18e38a6c202c5ebc36aee6abcd6ffd24e2ef256c
Nickolaus D. Saint <nick@globus.org>
2021-02-09 13:51:44 -0800

    Fix ACL bug, deploy flows for dev and staging

--- globus_automate/deploy.py
+++ globus_automate/deploy.py
@@ -65,7 +65,7 @@ def flow(service):
             group_dir = os.path.join(CFDE_CONFIG["LONG_TERM_STORAGE"], dcc_name) + "/"
             create_dir(group_dir)
             create_acl(group_dir, gid, "r")
-            create_acl("/CFDE/data/", gid, "w")
+            create_acl("/CFDE/data/", gid, "rw")
 
     globus_urns = list(set(globus_urns))
     full_flow_deploy_res = flows_client.deploy_flow(
